149fb6b641a583035854692e2c4d552ff98374cd
youngsoo-choi-scott <youngsu341@gmail.com>
2025-09-07 15:01:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const grid = document.querySelector(".grid");
-  const squareSize = 30; // 사각형 크기 (px)
-  const gapSize = 10; // 사각형 간의 여백 (px)
-  
+  const squareSize = 40; // 사각형 크기 (px)
+
   // 화면 전체를 채우기 위해 정사각형 크기와 간격 계산
-  const cols = Math.floor(window.innerWidth / (squareSize + gapSize));
-  const rows = Math.floor(window.innerHeight / (squareSize + gapSize));
+  const cols = Math.floor(window.innerWidth / squareSize);
+  const rows = Math.floor(window.innerHeight / squareSize);
   const totalSquares = cols * rows;
 
   // 정사각형 추가
